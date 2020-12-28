89b639f32b68b46368d4d27cf672b49a044116c6
matthewmjm <matthewmjm@yahoo.com>
2020-12-28 13:51:18 -0700

    change from state to props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import './App.css';
 import { Component } from 'react'
 import CharactersContainer from './components/CharactersContainer';
+import { connect } from 'react-redux'
 
 class App extends Component{
 
-  state = {
-    characters: []
+  // state = {
+  //   characters: []
+  // }
+  
+    componentDidMount(){
+      fetch("https://rickandmortyapi.com/api/character/")
+      .then(response => response.json())
+      .then(({results}) => this.props.setCharacters(results))
+    }
+  
+    render(){
+      return (
+        <div className="App">
+          <CharactersContainer characters={this.props.characters} />
+        </div>
+      );
+    }
   }
-
-  componentDidMount(){
-    fetch("https://rickandmortyapi.com/api/character/")
-    .then(response => response.json())
-    .then(({results}) => this.setState({characters: results}))
+  
+  const mapStateToProps = (state) => {
+    return {
+      characters: state.characters,
+      user: state.user
+    }
   }
-
-  render(){
-    return (
-      <div className="App">
-        <CharactersContainer characters={this.state.characters} />
-      </div>
-    );
+  
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      setCharacters: (characters) => dispatch({type: 'SET_CHARACTERS', payload: characters})
+    }
   }
-}
+  
+  export default connect(mapStateToProps, mapDispatchToProps)(App);
 
-export default App;
+// currying
+
+// const myFunction = (params1, param2) => (component) => connectedComponent
+
+// function myFunction(params1, params2){
+//   return function(component){
+//     return connectedComponent
+//   }
+// }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
-const store = createStore(() => {});
+const userReducer = (state={}, action) => {
+  switch(action.type){
+    default:
+      return state
+    }
+}
+  
+const charactersReducer = (state=[], action) => {
+  switch(action.type){
+    case 'SET_CHARACTERS':
+      return action.payload
+      case 'ADD_CHARACTER':
+        return [...state, action.character]
+        default:
+          return state
+  }
+}
+      
+const reducer = combineReducers({
+  user: userReducer,
+  characters: charactersReducer
+})
 
+
+const store = createStore(reducer);
+        
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
